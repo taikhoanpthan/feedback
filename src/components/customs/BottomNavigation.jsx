9d9d38bc812c88edbed1059c0d85a515d06354b4
taikhoanpthan <25182003md>
2026-07-14 07:43:26 +0700

    feat: enhance BottomNavigation to open external links in a new tab

--- src/components/customs/BottomNavigation.jsx
+++ src/components/customs/BottomNavigation.jsx
@@ -5,7 +5,7 @@ import {
   SettingOutlined,
 } from "@ant-design/icons";
 import { motion } from "framer-motion";
-import { useLocation, useNavigate } from "react-router-dom";
+import { Link, useLocation, useNavigate } from "react-router-dom";
 
 const menus = [
   {
@@ -24,9 +24,10 @@ const menus = [
     label: "Donate",
   },
   {
-    key: "/settings",
+    key: "https://todo-yakiuo.vercel.app/",
     icon: <SettingOutlined />,
     label: "Settings",
+    external: true,
   },
 ];
 
@@ -71,7 +72,13 @@ export default function BottomNavigation() {
           return (
             <button
               key={item.key}
-              onClick={() => navigate(item.key)}
+              onClick={() => {
+                if (item.key.startsWith("http")) {
+                  window.open(item.key, "_blank");
+                } else {
+                  navigate(item.key);
+                }
+              }}
               className="relative flex flex-1 justify-center py-0.5"
             >
               {active && (
@@ -120,9 +127,7 @@ export default function BottomNavigation() {
                   }
                 `}
               >
-                <span className="text-[20px] leading-none">
-                  {item.icon}
-                </span>
+                <span className="text-[20px] leading-none">{item.icon}</span>
 
                 <span className="text-[10px] font-medium leading-none">
                   {item.label}
